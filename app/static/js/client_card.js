d29fdd0f4689d2d30b2790251bf5db3ee7f2bd72
Ed <edward.bayliss@icloud.com>
2022-07-30 19:31:34 +0100

    fixed loop for clients display

--- app/static/js/client_card.js
+++ app/static/js/client_card.js
@@ -1,9 +1,9 @@
-function show_client() {
-    document.getElementById('full_client_card').style.display = "block";
-    document.getElementById('mini_client_card').style.display = "none";
+function show_client(mini_client_card, full_client_card) {
+    document.getElementById(full_client_card).style.display = "block";
+    document.getElementById(mini_client_card).style.display = "none";
 }
 
-function hide_client() {
-    document.getElementById('full_client_card').style.display = "none";
-    document.getElementById('mini_client_card').style.display = "block";
+function hide_client(mini_client_card, full_client_card) {
+    document.getElementById(full_client_card).style.display = "none";
+    document.getElementById(mini_client_card).style.display = "block";
 }
